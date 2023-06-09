1a9fef98bb45e06025a5a180b82f1457791f77df
Gkits-Gk <thegkaju@gmail.com>
2023-06-09 12:00:27 +0530

    Tested MySql Table Data

---
+++ prac1.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `prac2` (
+  `name` char(20) DEFAULT NULL,
+  `telugu` int(10) DEFAULT NULL,
+  `hindi` int(10) DEFAULT NULL,
+  `english` int(10) DEFAULT NULL,
+  `mathes` int(10) DEFAULT NULL,
+  `science` int(10) DEFAULT NULL,
+  `social` int(10) DEFAULT NULL,
+  `total` int(10) DEFAULT NULL
+);
